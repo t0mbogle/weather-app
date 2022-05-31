34f505a3b0f20c5df13a5909c65b8c87b2c67ff1
Thomas <thomas.bogle@outlook.com>
2022-05-31 16:20:15 +0100

    ForecastSummary snapshot test

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -3,5 +3,29 @@ import { render } from "@testing-library/react";
 import ForecastSummary from "../../components/ForecastSummary";
 
 describe("ForecastSummary", () => {
-  it("renders", () => {});
+  const validProps = {
+    date: 1111111,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+
+  it("renders correctly", () => {
+    // asFagment returns a fragment of a rendered component,
+    // stub props then get passed in
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+    // toMatchSnapshot ensures that a value matches the most recent snapshot.
+    // Takes a snapshot of the UI and compares it to the test.
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
